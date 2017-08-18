1b395e7810d7b1af2ea4a4c54738ee7a764e005c
e2755699 <e2755699@gmail.com>
2017-08-19 02:45:18 +0800

    refactor naming

---
+++ codewars_stringAverage/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codewars_stringAverage
+{
+    class Kata
+    {
+        private readonly string[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        internal string AverageString(string str)
+        {
+            var strNumbers = str.Split(' ');
+            if (IsGreaterThanNineOrIsEmpty(strNumbers))
+            {
+                return "n/a";
+            }
+
+            var average = (int)Math.Floor(strNumbers.Select((x, i) => Array.IndexOf(numbers, x)).Average());
+
+            return numbers[average];
+        }
+
+        private bool IsGreaterThanNineOrIsEmpty(string[] strNumbers)
+        {
+            return strNumbers.Select((x) => !numbers.Contains(x)).Contains(true);
+        }
+    }
+}
