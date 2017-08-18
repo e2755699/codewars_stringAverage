fe971e112916879ed1cc3df2c91b9e9ba583c966
e2755699 <e2755699@gmail.com>
2017-08-19 01:24:41 +0800

    first version

--- codewars_stringAverage/UnitTest1.cs
+++ codewars_stringAverage/UnitTest1.cs
@@ -7,8 +7,48 @@ namespace codewars_stringAverage
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InputIsEmpty_shouldReturn_n_div_a()
         {
+            var input = string.Empty;
+            var expect = "n/a";
+            AccumShouldBe(expect, input);
+        }
+
+        [TestMethod]
+        public void InputIsGreaterThanNine_shoudReturn_n_div_a()
+        {
+            var input = "ten";
+            var expect = "n/a";
+            AccumShouldBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_one_shouldReturn_one()
+        {
+            var input = "one";
+            var expect = "one";
+            AccumShouldBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_zero_nine_shouldReturn_four()
+        {
+            var input = "zero nine";
+            var expect = "four";
+            AccumShouldBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_zero_nine_five_two_shouldReturn_four()
+        {
+            var input = "zero nine five two";
+            var expect = "four";
+            AccumShouldBe(expect, input);
+        }
+
+        private static void AccumShouldBe(string expect, string input)
+        {
+            Assert.AreEqual(expect, new StringAverage().Accum(input));
         }
     }
 }
